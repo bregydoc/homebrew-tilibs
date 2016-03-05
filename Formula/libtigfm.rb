a0c3f4f5b1d5f8586d6394bbaff9a3ec06ac78d9
Johanna Cherry <johanna.cherry@songkick.com>
2016-03-05 14:46:46 -0600

    Add gfm package

---
+++ Formula/libtigfm.rb
@@ -0,0 +1,36 @@
+class Libtigfm < Formula
+  desc "Group file manager for connecting to Texas Instruments calculators"
+  homepage "http://lpg.ticalc.org/prj_gfm/"
+  url "http://www.ticalc.org/pub/unix/gfm.tar.gz"
+  version "0.1"
+  sha256 ""
+  
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "pkg-config"
+  depends_on "libtool"
+  depends_on "gtk"
+  depends_on "tilp"
+  
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--enable-libusb10"
+    system "make", "install"
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test libticables`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
